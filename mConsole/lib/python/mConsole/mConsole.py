5ffba0a093904578d1b3d58b9a9c624f7aa9d073
anviar <anviar@oib1.yugrusi.rt.ru>
2010-01-11 08:24:53 +0300

    Fix autors

--- mConsole/lib/python/mConsole/mConsole.py
+++ mConsole/lib/python/mConsole/mConsole.py
@@ -1,12 +1,19 @@
 import persistent
 from zope.app import pagetemplate
+from zope import interface, schema
+
 
 class base(persistent.Persistent):
     greeting = 'Hello'
     subject = 'world'
+    
+class IHelloWorld(interface.Interface):
+    greeting = schema.TextLine()
+    subject = schema.TextLine()
+
 
 class MessageView(object):
-
+    interface.implements(IHelloWorld)
     def message(self):
         return '%s %s!' % (self.context.greeting, self.context.subject)
 
